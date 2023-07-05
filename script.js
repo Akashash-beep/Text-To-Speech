8773af11f5293e4a5d7d1ce1cc2e6ce54640f13a
Akash Ragupathy <57954503+Akashash-beep@users.noreply.github.com>
2023-07-05 16:56:53 +0100

    commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-    let val = document.getElementById('textbox')
-    let x = val.value.toString();
-    const speech = new SpeechSynthesisUtterance(x);
-    window.speechSynthesis.speak(speech);
-
-});
